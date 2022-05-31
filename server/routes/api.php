d43fcfdc670e3d0805bfd595797875510146304b
Mohammed20201991 <mohamedabid092@gmail.com>
2022-05-31 02:56:16 +0200

    Use REST API

--- server/routes/api.php
+++ server/routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:sanctum')->resource('/events',EventController::class)->except(['create','edit']);
-
+Route::resource('/events',EventController::class)->except(['create','edit']);
+// Route::middleware('auth:sanctum')->resource('/events',EventController::class)->except(['create','edit']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
